d6c6ecea35edae934bfe5d6aae9095e5a7293735
sergioppires <sergio.abilio.pires@gmail.com>
2021-09-14 21:54:14 -0300

    code refactoring for Asniffer

--- src/main/kotlin/org/jetbrains/plugins/template/services/ASnifferService.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/services/ASnifferService.kt
@@ -1,22 +1,37 @@
 package org.jetbrains.plugins.template.services
 
 import com.github.phillima.asniffer.ASniffer
+import com.github.phillima.asniffer.model.AMReport
 import com.github.phillima.asniffer.output.json.d3hierarchy.classview.JSONReportCV
 import com.github.phillima.asniffer.output.json.d3hierarchy.packageview.JSONReportPV
 import com.github.phillima.asniffer.output.json.d3hierarchy.systemview.JSONReportSV
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
 
 
 class ASnifferService : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        if(e.project?.basePath != null){
+        runAsniffer(e)
+    }
+
+    fun runAsniffer(e: AnActionEvent) {
+        if (e.project?.basePath != null) {
             var basePath = e.project?.basePath
-            val report = ASniffer(basePath, basePath).collectSingle()
-            val dirPathResults: String = basePath + File.separator + "asniffer_results"
-            File(dirPathResults).mkdir()
+            val projectPath: Path = Paths.get(basePath + File.separator + ".idea" + File.separator + "asniffer")
+            val report: AMReport = ASniffer(basePath, basePath).collectSingle()
+
+            val directory = File(projectPath.toUri())
+            if (!directory.exists()) {
+                directory.mkdir()
+            }
+
+            val dirPathResults: String = Paths.get(projectPath.toString()).normalize().toString()
+
+            //Bug
             JSONReportCV().generateReport(report, dirPathResults)
             JSONReportSV().generateReport(report, dirPathResults)
             JSONReportPV().generateReport(report, dirPathResults)
