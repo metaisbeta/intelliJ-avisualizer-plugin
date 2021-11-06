e30a92c5e96d75e5d175b9368efc2b4da87ec634
sergioppires <sergio.abilio.pires@gmail.com>
2021-11-06 12:18:03 -0300

    avisualizer plugin update

---
+++ src/main/kotlin/org/jetbrains/plugins/template/actions/GFavoritesMenuAction.kt
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.template.actions
+
+import org.jetbrains.plugins.template.services.GivServiceSettings
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.project.DumbAware
+import com.intellij.ui.jcef.JBCefBrowser
+
+class GFavoritesMenuAction : DefaultActionGroup(), DumbAware {
+
+    override fun update(e: AnActionEvent) {
+        e.presentation.isEnabled = childrenCount <= 0
+        e.presentation.icon = AllIcons.Nodes.Favorite
+    }
+
+    fun updateView(settings: GivServiceSettings, jbCefBrowser: JBCefBrowser) {
+        removeAll()
+        addAll(settings.getFavorites().map { GFavoriteWebAction(it.first, it.second, jbCefBrowser) })
+    }
+
+}
+
+
+
+
